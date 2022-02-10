2519d9e8ab34f89c15733c4a965e31791177422a
wujt89 <wujt89@gmail.com>
2022-02-10 20:26:35 +0100

    first try

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def checkAndDrawRedCircles(circles, actual_img, is_empty):
         height, width = actual_img.shape[:2]
         blank_image = np.zeros((height, width, 3), dtype="uint8")
         for i in circles[0, :]:
-            x = 0 if ((i[0] - i[2]-5 <0) or (i[0] - i[2]-5>width)) else i[0] - i[2]-5
-            y = 0 if ((i[1] - i[2]-5 <0) or (i[1] - i[2]-5>height)) else i[1] - i[2]-5
-            x2 = width if ((i[0] + i[2]+5 <0) or (i[0] + i[2]+5>width)) else i[0] + i[2]+5
-            y2 = height if ((i[1] + i[2]+5 <0) or (i[1] + i[2]+5>height)) else i[1] + i[2]+5
+            x = 0 if ((i[0] - i[2] - 5 < 0) or (i[0] - i[2] - 5 > width)) else i[0] - i[2] - 5
+            y = 0 if ((i[1] - i[2] - 5 < 0) or (i[1] - i[2] - 5 > height)) else i[1] - i[2] - 5
+            x2 = width if ((i[0] + i[2] + 5 < 0) or (i[0] + i[2] + 5 > width)) else i[0] + i[2] + 5
+            y2 = height if ((i[1] + i[2] + 5 < 0) or (i[1] + i[2] + 5 > height)) else i[1] + i[2] + 5
             cv2.rectangle(actual_img, (x, y), (x2, y2), (0, 255, 0), 1)
             roi = actual_img[y:y2, x:x2]
             boundaries = [([30, 30, 50], [150, 150, 255])]
@@ -48,30 +48,102 @@ def checkAndDrawRedCircles(circles, actual_img, is_empty):
         cv2.waitKey(0)
 
 
-def learn(folder):
-    number = 0
-    for filename in glob.glob(f'{folder}/*.xml'):
+def load(xmlFolder):
+    data = []
+    for filename in glob.glob(f'{xmlFolder}/*.xml'):
         doc = minidom.parse(filename)
-        n = 0
-        values = []
+        imgName = doc.getElementsByTagName("filename")[0].firstChild.data
         objects = doc.getElementsByTagName("object")
+        height = doc.getElementsByTagName("height")[0].firstChild.data
+        width = doc.getElementsByTagName("width")[0].firstChild.data
+        boxes = []
         for obj in objects:
-            name = obj.getElementsByTagName("name")[0]
-            if name.firstChild.data == 'speedlimit':
-                n+=1
-                xmin = obj.getElementsByTagName("xmin")[0].firstChild.data
-                ymin = obj.getElementsByTagName("ymin")[0].firstChild.data
-                xmax = obj.getElementsByTagName("xmax")[0].firstChild.data
-                ymax = obj.getElementsByTagName("ymax")[0].firstChild.data
-                val = [xmin, xmax, ymin, ymax]
-                values.append(val)
-        if n>0:
-            number+=1
-            print(f'{filename[16:len(filename)-4]}.png')
-            print(n)
-            for v in values:
-                print(v[0], v[1], v[2], v[3])
-    print(number)
+            typeOfSign = obj.getElementsByTagName("name")[0].firstChild.data
+            xmin = obj.getElementsByTagName("xmin")[0].firstChild.data
+            ymin = obj.getElementsByTagName("ymin")[0].firstChild.data
+            xmax = obj.getElementsByTagName("xmax")[0].firstChild.data
+            ymax = obj.getElementsByTagName("ymax")[0].firstChild.data
+            boxes.append({"typeOfSign": typeOfSign, "xmin": xmin, "xmax": xmax, "ymin": ymin, "ymax": ymax})
+        data.append({'imageName': imgName, 'height': height, 'width': width, "label": "1", "boxes": boxes})
+    return data
+
+
+def learn(data):
+    for element in data:
+        bow = cv2.BOWKMeansTrainer(37)
+        sift = cv2.SIFT_create()
+        img = cv2.imread(f'trainImages/{element["imageName"]}')
+        boxes = element['boxes']
+        for box in boxes:
+            roi = img[int(box["ymin"]):int(box["ymax"]), int(box["xmin"]):int(box["xmax"])]
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            kp = sift.detect(gray, None)
+            kp, desc = sift.compute(gray, kp)
+            roi = cv2.drawKeypoints(gray, kp, roi)
+            img[int(box["ymin"]):int(box["ymax"]), int(box["xmin"]):int(box["xmax"])] = roi
+            if desc is not None:
+                bow.add(desc)
+        # cv2.imshow("roi", img)
+        # cv2.waitKey(0)
+    vocabulary = bow.cluster()
+    np.save('voc.npy', vocabulary)
+
+    #     values = []
+    #     objects = doc.getElementsByTagName("object")
+    #     for obj in objects:
+    #         name = obj.getElementsByTagName("name")[0]
+    #         if name.firstChild.data == 'speedlimit':
+    #             n += 1
+    #             xmin = obj.getElementsByTagName("xmin")[0].firstChild.data
+    #             ymin = obj.getElementsByTagName("ymin")[0].firstChild.data
+    #             xmax = obj.getElementsByTagName("xmax")[0].firstChild.data
+    #             ymax = obj.getElementsByTagName("ymax")[0].firstChild.data
+    #             val = [xmin, xmax, ymin, ymax]
+    #             values.append(val)
+    #     if n>0:
+    #         number+=1
+    #         print(f'{filename[16:len(filename)-4]}.png')
+    #         print(n)
+    #         for v in values:
+    #             print(v[0], v[1], v[2], v[3])
+    # print(number)
+
+
+def extract(data, path):
+    dictSize = 37
+    sift = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(sift, flann)
+    vocabulary = np.load('voc.npy')
+    bow.setVocabulary(vocabulary)
+    for element in data:
+        img = cv2.imread(f'{path}{element["imageName"]}')
+        kp = sift.detect(img, None)
+        desc = bow.compute(img, kp)
+        if desc is not None:
+            element.update({'desc': desc})
+        else:
+            element.update({'desc': np.zeros((1, 37))})
+    return data
+
+
+def train(data):
+    clf = RandomForestClassifier(37)
+    x_matrix = np.empty((1, 37))
+    y_vector = []
+    for element in data:
+        y_vector.append(element['label'])
+        x_matrix = np.vstack((x_matrix, element['desc']))
+    clf.fit(x_matrix[1:], y_vector)
+    return clf
+
+
+def predict(rf, data):
+    for sample in data:
+        sample.update({'label_pred': rf.predict(sample['desc'])[0]})
+    # ------------------
+
+    return data
 
 
 def main(folder):
@@ -111,5 +183,20 @@ if __name__ == '__main__':
     #         shutil.copy2(filename, 'trainAnnotations')
     #     else:
     #         shutil.copy2(filename, 'testAnnotations')
-    learn("testAnnotations")
-    main("testImages")
+
+
+    train_data = load("trainAnnotations")
+    test_data = load("testAnnotations")
+    print("learn")
+    learn(train_data)
+    print("extracting train")
+    train_data = extract(train_data, "trainImages/")
+    print("train")
+    rf = train(train_data)
+    print("extracting test")
+    test_data = extract(test_data, "testImages/")
+    print("testing")
+    predict(rf, test_data)
+    print("done")
+
+    # main("testImages")
